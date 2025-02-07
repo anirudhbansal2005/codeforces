881176d240e62a1bfdde7dc01d38607762fd0af2
Anirudh Bansal <anirudhbansal005@gmail.com>
2025-02-07 22:42:40 +0530

    i

--- C/1669C.cpp
+++ C/1669C.cpp
@@ -2,13 +2,15 @@
 using namespace std;
 
 bool foo(const vector<int>& v, const int& x){
+    int even_parity = v[0]%2;
+    int odd_parity = v[1]%2;
     for(int i=0; i<x; i+=2){
-        if((v[i])!=v[0]){
+        if((v[i]%2)!=even_parity){
             return false;
         }
     }
     for(int i=1; i<x; i+=2){
-        if((v[i])!=v[1]){
+        if((v[i]%2)!=odd_parity){
             return false;
         }
     }
