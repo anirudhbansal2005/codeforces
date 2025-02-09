6e213ce6d434ddc5d71aaae0520b5b6edeb84a72
Anirudh Bansal <anirudhbansal005@gmail.com>
2025-02-09 15:03:49 +0530

    2

--- A/271A.cpp
+++ A/271A.cpp
@@ -1,26 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
+int distinctYear(int& x){
+    for(int i=1; i>0; i++){
+        string r = to_string(x);
+        set<char> y(r.begin(), r.end());
+        if(y.size()!=4){
+            x++;
+        }
+        else {
+            break;
+        }
+    }
+    return x;
+}
 int main(){
     int year;
     cin >> year;
-    string yr="";
-    bool y=0;
-    int length = 0;
-    for(int i=year; i>0; i++){
-        year++;
-        length = to_string(year).size();
-        yr = to_string(year);
-        for(int j=0; j<length; j++){
-            if(yr[0]!=yr[j]){
-                y=1;
-            }
-            else{
-                y=0;
-            }
-        }
-    if(y){
-        cout<<year;
-        break;
-    }
-    }
+    year++;
+    cout<<distinctYear(year);
 }
