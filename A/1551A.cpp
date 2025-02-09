36b05d96533594feaabd22b58d7c25eb17bf68fc
Anirudh Bansal <anirudhbansal005@gmail.com>
2025-02-09 17:12:01 +0530

    3

--- A/1551A.cpp
+++ A/1551A.cpp
@@ -1,32 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
+void buriesCalc(long long x){
+    long long bury_1 = x/3;
+    long long bury_2 = x/3;
+    if(x%3==1){
+        bury_1++;
+    }
+    else if(x%3==2){
+        bury_2++;
+    }
+    cout<<bury_1<<" "<<bury_2<<"\n";
+}
 int main(){
     int cases;
     cin >> cases;
-    int total;
-    int c1, c2;
-    float calc;
+    long long total;
     for(int i=0; i<cases; i++){
-        cin >> total;
-        calc = (float)total/3)%1);
-        cout<<calc<<"\n";
-        if(calc==0){
-            c1 = total/3;
-            c2 = c1;
-        }
-        else if (calc!=0){
-            
-            c1 = total/3;
-            cout<<(float)total/3;
-            c2 = c1;
-            if(c1+(2*c2)!=total){
-                c1=c1-1;
-                c2=c2;
-            }
-        }
-    cout<<c1<<" "<<c2<<"\n";
-    c1=0;
-    c2=0;
+       cin >> total;
+       buriesCalc(total);
     }
-    
 }
